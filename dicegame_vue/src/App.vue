1341722ec1f67d392ad8df9f34c03c7b870c7b79
tung.pham@ <tung.pham@pacom-solution.com>
2022-10-23 15:23:39 +0700

    final dice game

--- dicegame_vue/src/App.vue
+++ dicegame_vue/src/App.vue
@@ -1,60 +1,164 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="wrapper clearfix">
+      <PlayerComponent
+        v-bind:scorePlayer="scorePlayer"
+        v-bind:activePlayer="activePlayer"
+        v-bind:currentScore="currentScore"
+        v-bind:isWinner="isWinner"
+      />
+      <ControlComponent
+        v-on:handleNewGame="handleNewGame"
+        v-on:handleRollDice="handleRollDice"
+        v-on:handleHold="handleHold"
+        v-bind:isPlaying="isPlaying"
+        v-bind:finalScore="finalScore"
+        v-on:handleChangeFinalScore="handleChangeFinalScore"
+      />
+      <DiceComponent v-bind:dices="dices" />
+      <PopupRuleComponent
+        v-bind:isOpenPopup="isOpenPopup"
+        v-on:handleConfirm="handleConfirm"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import PlayerComponent from "./Components/PlayerComponent.vue";
+import ControlComponent from "./Components/ControlComponent.vue";
+import DiceComponent from "./Components/DiceComponent.vue";
+import PopupRuleComponent from "./Components/PopupRuleComponent.vue";
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      isPlaying: false,
+      isOpenPopup: false,
+      activePlayer: 0, // 0:active đang chơi, 1: ngta chơi
+      scorePlayer: [0, 0],
+      dices: [1, 5],
+      currentScore: 0,
+      finalScore: ''
+    };
+  },
+  components: {
+    PlayerComponent,
+    ControlComponent,
+    DiceComponent,
+    PopupRuleComponent,
+  },
+  computed: {
+    isWinner() {
+      let { scorePlayer, finalScore} = this;
+      if(scorePlayer[0] >= finalScore || scorePlayer[1] >= finalScore) {
+        // dừng cuôc chơi
+        this.isPlaying = false;
+        return true;
+      }
+      return false;
     }
-  }
-}
+  },
+  methods: {
+    handleConfirm() {
+      this.isPlaying = true;
+      this.isOpenPopup = false;
+      this.activePlayer = 0;
+      this.scorePlayer = [0, 0];
+      this.dices = [1, 1];
+      this.currentScore = 0;
+    },
+    handleNewGame() {
+      this.isOpenPopup = true;
+    },
+    nextPlayer() {
+      this.activePlayer = this.activePlayer == 0 ? 1 : 0; 
+      this.currentScore = 0
+    },
+    handleRollDice() {
+      if (this.isPlaying) {
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+        this.dices = [dice1, dice2];
+
+        if (dice1 == 1 || dice2 == 1) {
+          let activePlayer = this.activePlayer;
+          // đổi lượt chơi
+          setTimeout(function () {
+            alert(`Người chơi Player ${activePlayer + 1} đã quay trúng số 1. Rất tiếc!`)
+          }, 1000)
+          this.nextPlayer()
+        } else {
+          this.currentScore = this.currentScore + dice1 + dice2;
+        }
+      } else {
+        alert("Vui long chon new game");
+      }
+    },
+    handleHold() { 
+      if(this.isPlaying) { 
+        let { scorePlayer, activePlayer, currentScore } = this
+        this.$set(this.scorePlayer, activePlayer, scorePlayer[activePlayer] + currentScore)
+        // let cloneScorePlayer = [...scorePlayer];
+        //   cloneScorePlayer[activePlayer] = scorePlayer[activePlayer] + currentScore
+
+        // // this.scorePlayer[activePlayer] = scorePlayer[activePlayer] + currentScore
+        // this.scorePlayer = cloneScorePlayer 
+        if(!this.isWinner) {
+          this.nextPlayer()
+        }
+      }else {
+        alert("Vui long chon new game");
+      }
+    },
+    handleChangeFinalScore(event) { 
+      var number = parseInt(event.target.value)
+      if(isNaN(number)) {
+        this.finalScore = ''
+      } else {
+        this.finalScore = number
+      }
+    }
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
+* {
+  margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.clearfix::after {
+  content: "";
+  display: table;
+  clear: both;
 }
 
-a {
-  color: #42b983;
+body {
+  background-image: linear-gradient(
+      rgba(62, 20, 20, 0.4),
+      rgba(62, 20, 20, 0.4)
+    ),
+    url("/public/assets/back.jpg");
+  background-size: cover;
+  background-position: center;
+  font-family: Lato;
+  font-weight: 300;
+  position: relative;
+  height: 100vh;
+  color: #555;
+}
+
+.wrapper {
+  width: 1000px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  box-shadow: 0px 10px 50px rgba(0, 0, 0, 0.3);
+  overflow: hidden;
 }
 </style>
---
+++ dicegame_vue/src/Components/PopupRuleComponent.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="wrapper_popup" :class="getClassPopup">
+    <div class="rule">
+      <h3>Luật chơi</h3>
+      <p>Lorem ipsum dolor sit amet, consectet</p>
+      <button class="confirm" @click="confirm">Đã hiểu</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "popup-rule",
+  data() {
+    return {
+
+    }
+  },
+  props: {
+    isOpenPopup: {
+        type: Boolean,
+        default: false
+    }
+  },
+  methods: {
+    confirm() {
+        this.$emit('handleConfirm')
+    }
+  },
+  computed: {
+    getClassPopup: function () {
+        return {
+            'open-popup': this.isOpenPopup
+        }
+    }
+  }
+};
+</script>
+
+<style>
+.wrapper_popup {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 0;
+  visibility: hidden;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+.wrapper_popup.open-popup {
+    opacity: 1;
+    visibility: visible;
+}
+.rule {
+  width: 350px;
+  padding: 10px;
+  position: absolute;
+  background-color: white;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.rule h3 {
+  margin-bottom: 10px;
+}
+.rule button {
+  margin-top: 10px;
+}
+.rule .confirm {
+  border: 2px solid #333;
+  padding: 5px;
+  background-color: white;
+  cursor: pointer;
+}
+</style>
